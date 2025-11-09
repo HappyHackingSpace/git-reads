f061b1626e10578b1072a3d6edba6d79150559d3
HazarBakir <hazar_bakir@protonmail.com>
2025-11-10 01:23:46 +0300

    refactor: restructure App component layout and update styles
    - Updated App component to include a new grid layout with Header and main content areas.
    - Added CSS styles for app-root-grid, document-header, and main-content to enhance layout structure.
    - Replaced the favicon with a new logo image (papyr-logo.png) for improved branding.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing/Landing";
 import Document from "./pages/Document/Document";
+import Header from './components/Header';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route index element={<Landing />} />
-        <Route path="/document" element={<Document />} />
-      </Routes>
-    </>
+    <div className="app-root-grid">
+      <Header />
+      <main className="main-content">
+        <Routes>
+          <Route index element={<Landing />} />
+          <Route path="/document" element={<Document />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
 export default App;
+

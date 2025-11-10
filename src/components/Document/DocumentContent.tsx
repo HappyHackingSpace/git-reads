3b60c8d3252f3d37410470a827cb83c2ed5880b1
HazarBakir <hazar_bakir@protonmail.com>
2025-11-10 15:32:32 +0300

    refactor: relocate Documentcontent component

---
+++ src/components/Document/DocumentContent.tsx
@@ -0,0 +1,22 @@
+import { DocumentMarkdown } from "./DocumentMarkdown";
+
+export function DocumentContent({
+  loading,
+  error,
+  markdown,
+}: {
+  loading: boolean;
+  error: string | null;
+  markdown: string;
+}) {
+  if (loading) {
+    return <div className="document-loading">Loading...</div>;
+  }
+  if (error) {
+    return <div className="document-error">Error: {error}</div>;
+  }
+  if (!markdown) {
+    return <div className="document-empty">No content available.</div>;
+  }
+  return <DocumentMarkdown markdown={markdown} />;
+}
